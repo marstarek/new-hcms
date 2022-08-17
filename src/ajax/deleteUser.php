1d18554100ecdb2fb63477ea4c7854534e204102
marstarek <tarekahmed1568@gmail.com>
2022-08-17 21:51:29 +0200

    update

--- src/ajax/deleteUser.php
+++ src/ajax/deleteUser.php
@@ -22,24 +22,24 @@ if (isset($_POST['deleteUser'])) {
 }
 else if (isset($_POST['editRow'])) {
      $userID = $_POST['userID'];
-     $Username = $_POST['Username'];
-     $DisplayName = $_POST['DisplayName'];
-     $EmployeeID = $_POST['EmployeeID'];
-     $email = $_POST['email'];
-     $password = $_POST['password'];
-     $mac = $_POST['mac'];
-     echo $EmployeeID;
-     // $isAdmin = $_POST['isAdmin'];
+     $Username =mysqli_escape_string($conn,$_POST['Username']);
+     $DisplayName = mysqli_escape_string($conn,$_POST['DisplayName']);
+     $EmployeeID =mysqli_escape_string($conn,$_POST['EmployeeID']) ;
+     $email =mysqli_escape_string($conn,$_POST['email']) ;
+     $userpassword =md5( $_POST['password']);
+     $mac =mysqli_escape_string($conn,$_POST['mac']) ;
+     $isAdmin = $_POST['isAdmin'];
+     echo $Username;
      // $isActive = $_POST['isActive'];
  
     $query = "SELECT *  FROM `users` WHERE EmployeeID = $userID";
     $result = mysqli_query($conn, $query);
     if($result->num_rows == 1) {
-$editQuery = " UPDATE `users` SET DisplayName='$DisplayName',Username='$Username' WHERE EmployeeID = $userID";
+$editQuery = " UPDATE `users` SET DisplayName='$DisplayName',Username='$Username' ,Email='$email',	Password='$userpassword' WHERE EmployeeID = $userID";
     $result = mysqli_query($conn, $editQuery);
     echo json_encode([
      'status' => 200,
-     'message' => 'User Deleted Successfully !'
+     'message' => 'User updated Successfully !'
 ]);
 // echo $query;
     } else {
